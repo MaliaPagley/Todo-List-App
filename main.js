2bef0dc8e50b262ab958eaf0b1babc9eba4cee8c
MaliaPagley <maliapagley@gmail.com>
2023-04-25 21:16:57 -0400

    Added functionality with js

--- main.js
+++ main.js
@@ -51,6 +51,20 @@ window.addEventListener('load', () => {
 
     list_el.appendChild(task_el);
 
-    input.value =""
+    input.value = "";
+
+    task_edit_el.addEventListener('click', () => {
+       if (task_edit_el.innerText.toLowerCase() == "edit") {
+            task_input_el.removeAttribute("readonly");
+            task_input_el.focus();
+            task_edit_el.innerText = "Save";
+               } else {
+                task_input_el.setAttribute("readonly", "readonly");
+                task_edit_el.innerText = "Edit";
+               }       
+    });
+    task_delete_el.addEventListener('click', () => {
+        list_el.removeChild(task_el);
+    });
   })
 })
