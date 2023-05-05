c676706765b247d820993d936211c2fc7e26eb66
MaliaPagley <maliapagley@gmail.com>
2023-05-04 20:09:46 -0400

    toast notification added

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ window.addEventListener('load', () => { // (<) On load all the code inside this 
     const form = document.querySelector('#new-task-form');
     const input = document.querySelector('#new-task-input');
     const list_el = document.querySelector('#tasks');
+    const toast = document.getElementById("toast");
 
   form.addEventListener('submit', (e) => { //(<) When form is submitted all the code inside it will execute.
     e.preventDefault();
@@ -32,9 +33,16 @@ window.addEventListener('load', () => { // (<) On load all the code inside this 
         } else {
             check_el.classList.add("checked")
             strike()
+            showToast()
         }
     })
-   
+   function showToast() {
+    const toast = document.getElementById("toast");
+        toast.className = toast.className + " show";
+   }
+    setTimeout(function () {
+        toast.className = toast.className.replace(" show", " ");
+    }, 3500);
 
     const task_content_el = document.createElement("div");
     task_content_el.classList.add("content");
